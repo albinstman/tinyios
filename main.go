ccdc36677a24573de1313560e296465f0e5066bf
albinmini <albin.ostman98@gmail.com>
2025-11-16 22:58:01 +0000

    readme

--- main.go
+++ main.go
@@ -17,6 +17,13 @@ import (
 	"github.com/danielpaulus/go-ios/ios/tiny"
 )
 
+// @title           tinyios
+// @version         0.0.1
+
+type GenericResponse struct {
+	OK bool `json:"ok"`
+}
+
 //go:embed c.der
 var cder []byte
 
@@ -87,126 +94,338 @@ func deviceMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func writeResponse(w http.ResponseWriter, data []byte) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+// reboot godoc
+// @Summary      Reboot device
+// @Description  Reboots the specified iOS device
+// @Tags         device
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/reboot [post]
 func reboot(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.Reboot(d))
-	w.Write(result)
+	writeResponse(w, result)
 }
 
+// activated godoc
+// @Summary      Check activation status
+// @Description  Returns whether the device is activated
+// @Tags         activation
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/activated [get]
 func activated(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.Activated(d))
 	w.Write(result)
 }
 
+// activateEnable godoc
+// @Summary      Enable activation
+// @Description  Activates the specified iOS device
+// @Tags         activation
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/activate/enable [post]
 func activateEnable(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.ActivateEnable(d))
 	w.Write(result)
 }
 
+// supervised godoc
+// @Summary      Check supervision status
+// @Description  Returns whether the device is supervised
+// @Tags         supervision
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/supervised [get]
 func supervised(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.Supervised(d))
 	w.Write(result)
 }
 
+// superviseEnable godoc
+// @Summary      Enable supervision
+// @Description  Prepares and enables supervision on the device
+// @Tags         supervision
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/supervise/enable [post]
 func superviseEnable(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.Prepare(d, cder, "tinyios", "en-US", "en"))
 	w.Write(result)
 }
 
+// erase godoc
+// @Summary      Erase device
+// @Description  Erases all content and settings from the device
+// @Tags         device
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/erase [post]
 func erase(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.Erase(d))
 	w.Write(result)
 }
 
+// paired godoc
+// @Summary      Check pairing status
+// @Description  Returns whether the device is paired
+// @Tags         pairing
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/paired [get]
 func paired(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.Paired(d))
 	w.Write(result)
 }
 
+// pairEnable godoc
+// @Summary      Enable pairing
+// @Description  Pairs the device using the provided certificate
+// @Tags         pairing
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/pair/enable [post]
 func pairEnable(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.PairEnable(d, p12))
 	w.Write(result)
 }
 
+// devmode godoc
+// @Summary      Check developer mode status
+// @Description  Returns whether developer mode is enabled on the device
+// @Tags         developer
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/devmode [get]
 func devmode(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.Devmode(d))
 	w.Write(result)
 }
 
+// devmodeEnable godoc
+// @Summary      Enable developer mode
+// @Description  Enables developer mode on the device
+// @Tags         developer
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/devmode/enable [post]
 func devmodeEnable(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.DevmodeEnable(d))
 	w.Write(result)
 }
 
+// image godoc
+// @Summary      Check developer disk image status
+// @Description  Returns whether the developer disk image is mounted
+// @Tags         developer
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/image [get]
 func image(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.Image(d))
 	w.Write(result)
 }
 
+// imageEnable godoc
+// @Summary      Mount developer disk image
+// @Description  Mounts the developer disk image on the device
+// @Tags         developer
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/image/enable [post]
 func imageEnable(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.ImageEnable(d))
 	w.Write(result)
 }
 
+// profileList godoc
+// @Summary      List profiles
+// @Description  Returns a list of configuration profiles installed on the device
+// @Tags         profiles
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/profiles/list [get]
 func profileList(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.ProfileList(d))
 	w.Write(result)
 }
 
+type ProfilleAddRequset struct {
+	B64Profile string `json:"b64profile"`
+}
+
+// profileAdd godoc
+// @Summary      Add profile
+// @Description  Installs a configuration profile on the device
+// @Tags         profiles
+// @Accept       json
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Param        profile body ProfilleAddRequset true "Base64 encoded profile"
+// @Success      201 {object} map[string]string
+// @Router       /{udid}/profiles/add [post]
 func profileAdd(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
+
+	// Decode JSON
+	var u ProfilleAddRequset
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields() // catch unwanted fields
+
+	if err := decoder.Decode(&u); err != nil {
+		http.Error(w, "invalid JSON: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Optional: ensure no extra JSON after the first object
+	if decoder.More() {
+		http.Error(w, "invalid JSON: multiple objects", http.StatusBadRequest)
+		return
+	}
+
+	// Do something with u (e.g., save to DB)
+	log.Printf("Got user: %+v\n", u)
+
+	// Return a response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	resp := map[string]string{"status": "ok"}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		log.Printf("error writing response: %v", err)
+	}
+
 	result := []byte(tiny.ProfileAdd(d, []byte(r.FormValue("b64profile")), p12))
 	w.Write(result)
 }
 
+// appList godoc
+// @Summary      List applications
+// @Description  Returns a list of applications installed on the device
+// @Tags         apps
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/apps/list [get]
 func appList(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.AppList(d))
 	w.Write(result)
 }
 
+// appRun godoc
+// @Summary      Run application
+// @Description  Launches an application on the device
+// @Tags         apps
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Param        bundleid formData string true "Application bundle identifier"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/apps/run [post]
 func appRun(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.AppRun(d, r.FormValue("bundleid")))
 	w.Write(result)
 }
 
+// appInstall godoc
+// @Summary      Install application
+// @Description  Installs an application from a URL on the device
+// @Tags         apps
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Param        url formData string true "Application IPA URL"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/apps/install [post]
 func appInstall(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.AppInstall(d, r.FormValue("url")))
 	w.Write(result)
 }
 
+// appKill godoc
+// @Summary      Kill application
+// @Description  Terminates a running application by process ID
+// @Tags         apps
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Param        pid formData string true "Process ID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/apps/kill [post]
 func appKill(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.AppKill(d, r.FormValue("pid")))
 	w.Write(result)
 }
 
+// processes godoc
+// @Summary      List processes
+// @Description  Returns a list of running processes on the device
+// @Tags         device
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/processes [get]
 func processes(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.Processes(d))
 	w.Write(result)
 }
 
+// wdaRun godoc
+// @Summary      Run WebDriverAgent
+// @Description  Starts WebDriverAgent on the device
+// @Tags         wda
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/wda/run [post]
 func wdaRun(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.WdaRun(d))
 	w.Write(result)
 }
 
+// wdaKill godoc
+// @Summary      Kill WebDriverAgent
+// @Description  Stops WebDriverAgent on the device
+// @Tags         wda
+// @Produce      json
+// @Param        udid   path      string  true  "Device UDID"
+// @Success      200 {object} GenericResponse
+// @Router       /{udid}/wda/kill [post]
 func wdaKill(w http.ResponseWriter, r *http.Request) {
 	d, _ := getDevice(r.Context())
 	result := []byte(tiny.WdaKill(d))
--- markdown.md
+++ markdown.md
@@ -1,92 +1,5 @@
-# tinyios
 
-[![Go Report Card](https://goreportcard.com/badge/github.com/albinstman/tinyios)](https://goreportcard.com/report/github.com/albinstman/tinyios) ![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/albinstman/tinyios?sort=semver)
 
-----
-
-tinyios is cross platform software to talk to ios devices over usb or wifi. It mimics a small set of features that can be done via xcode, developer tools and system settings on a mac.
-
-It exposes itself as a HTTP server so it can be accessed from anywhere. It is also stateless and rootless which makes it easy to run as an ephemeral container.
-
-tinyios depends on usbmuxd installed on the host machine to manage device pairing and device communication through usb or wifi. You will also need to make usbmuxd on the host available inside the container.
-
-----
-## Installation
-
-### WSL
-
-1. Forward device to WSL
-2. Install dependancies
-
-    To use tinyios on Linux or WSL you need to install usbmuxd to handle device pairing and device communication. You can run these commands to install or update existing usbmuxd to the latest version
-
-    ```
-    git clone https://github.com/libimobiledevice/usbmuxd.git
-    cd usbmuxd
-    ./autogen.sh
-    make
-    sudo make install
-    ```
-    You will also need socat to make usbmuxd available from the host to container
-    ```
-    sudo apt install socat
-    sudo socat TCP-LISTEN:27015,reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd
-    ```
-3. Run latest container image on port 8080
-    ```
-    docker run —rm \
-    -p 8080:80 \
-    -e USBMUXD_SOCKET_ADDRESS=host.docker.internal:27015 \
-    albinstman/tinyios
-    ```
-
-### Linux
-
-1. Install dependancies
-
-    To use tinyios on Linux or WSL you need to install usbmuxd to handle device pairing and device communication. You can run these commands to install or update existing usbmuxd to the latest version
-
-    ```
-    git clone https://github.com/libimobiledevice/usbmuxd.git
-    cd usbmuxd
-    ./autogen.sh
-    make
-    sudo make install
-    ```
-    You will also need socat to make usbmuxd available from the host to container
-    ```
-    sudo apt install socat
-    sudo socat TCP-LISTEN:27015,reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd
-    ```
-2. Run latest container image on port 8080
-    ```
-    docker run —rm \
-    -p 8080:80 \
-    -e USBMUXD_SOCKET_ADDRESS=host.docker.internal:27015 \
-    albinstman/tinyios
-    ```
-
-### Mac
-
-1. Make usbmuxd available for container
-    ```
-    brew install socat
-    socat TCP-LISTEN:27015,reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd
-    ```
-2. Run latest container image on port 8080
-    ```
-    docker run —rm \
-    -p 8080:80 \
-    -e USBMUXD_SOCKET_ADDRESS=host.docker.internal:27015 \
-    albinstman/tinyios
-    ```
-
-----
-
-## Purpose 
-The main purpose of tinyios is to setup ios devices and then talk to them via appium webdriver commands.
-
-----
 
 # tinyios
   
@@ -96,6 +9,8 @@ The main purpose of tinyios is to setup ios devices and then talk to them via ap
 ### Version
 
 0.0.1
+
+### Contact
 
   
 
@@ -941,3 +856,5 @@ Status: OK
 | Name | Type | Go type | Required | Default | Description | Example |
 |------|------|---------|:--------:| ------- |-------------|---------|
 | b64profile | string| `string` |  | |  |  |
+
+
